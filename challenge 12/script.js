e33ce65eb1be908d1d3b51887f5e8d4ca26a56ed
10062005 <93771723+10062005@users.noreply.github.com>
2022-06-20 12:51:22 +0200

    Add files via upload

---
+++ challenge 12/script.js
@@ -0,0 +1,228 @@
+
+// ADD time?
+
+var snakeTable = document.querySelector(".snakeTable");
+var boxes = document.getElementsByClassName("box");
+var modul = document.querySelector(".modul");
+var start = document.querySelector(".start");
+
+var table = {
+  rowsCols: 21,
+  boxes: 21*21
+};
+
+var snake = {
+  direction: "right",
+  position: [[6,10], [7,10], [8,10], [9,10], [10,10]],
+  interval: 200,
+  food: 0,
+  score: 0,
+  final: 0,
+  time: 0,
+  canTurn: 0,
+  init: function() {
+    snake.direction = "right";
+    snake.position = [[6,10], [7,10], [8,10], [9,10], [10,10]];
+    snake.interval = 200;
+    snake.food = 0;
+    snake.score = 0;
+    snake.time = 0;
+    snake.canTurn = 0;
+    snakeTable.innerHTML = "";
+    tableCreation();
+  }
+};
+
+// init game
+snake.init();
+
+start.addEventListener("click", startSnake);
+
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode === 13 && snake.time === 0) {
+    startSnake();
+  }
+});
+
+// start game
+function startSnake() {
+  modul.classList.add("hidden");
+  // clearInterval(checkPageInterval);
+  snake.time = 1;
+  renderSnake();
+  randomFood();
+  // interval, heart of the game
+  setInt = setInterval(function() {
+    move();
+  }, snake.interval);
+}
+
+// end of game
+function stopp() {
+  clearInterval(setInt);
+  snake.final = snake.score;
+  start.querySelector("span").innerHTML = snake.final + " Points !";
+  setTimeout(function() {
+    start.querySelector("span").innerHTML = "Play Snake";
+  }, 1500);
+  snake.init();
+  modul.classList.remove("hidden");
+}
+
+// move the snake function
+function move() {
+  // check if move allowed & then eat food
+  hitFood();
+  hitBorder();
+  hitSnake();
+  // actually move the snake
+  updatePositions();
+  renderSnake();
+  document.addEventListener("keydown", turn);
+  snake.canTurn = 1;
+}
+
+function updatePositions() {
+  // remove last snake part (first snake pos)
+  boxes[snake.position[0][0] + snake.position[0][1] * table.rowsCols].classList.remove("snake");
+  snake.position.shift();
+  // add new snake part
+  var head = snake.position[snake.position.length - 1];
+  switch (snake.direction) {
+    case "left":
+      snake.position.push([head[0] - 1, head[1]]);
+      break;
+    case "up":
+      snake.position.push([head[0], head[1] - 1]);
+      break;
+    case "right":
+      snake.position.push([head[0] + 1, head[1]]);
+      break;
+    case "down":
+      snake.position.push([head[0], head[1] + 1]);
+      break;
+    default:
+      console.log("no direction !");
+  }
+}
+
+
+
+// checks food contact
+function hitFood() {
+  var head = snake.position[snake.position.length-1];
+  var tail = snake.position[0];
+  if (head.toString() === foodPos.toString()) {
+    boxes[random].classList.remove("food");
+    snake.position.unshift(tail);
+    randomFood();
+    snake.food++;
+    snake.score += snake.food;
+    scoreElt.innerHTML = snake.score + " pts";
+    // increase speed
+    clearInterval(setInt);
+    snake.interval = snake.interval - snake.interval/40;
+    setInt = setInterval(function() {
+      move();
+    }, snake.interval);
+  }
+}
+
+
+// read positions and render the snake
+function renderSnake() {
+  for (var i=0; i<snake.position.length; i++) {
+    boxes[snake.position[i][0] + snake.position[i][1] * table.rowsCols].classList.add("snake");
+  }
+}
+
+// keypress handling to turn
+function turn(e) {
+  if (snake.canTurn) {
+    switch (e.keyCode) {
+      case 13:
+        // document.removeEventListener()
+        break;
+      case 37:// left
+        if (snake.direction === "right") return;
+        snake.direction = "left";
+        break;
+      case 38:// up
+        if (snake.direction === "down") return;
+        snake.direction = "up";
+        break;
+      case 39:// right
+        if (snake.direction === "left") return;
+        snake.direction = "right";
+        break;
+      case 40:// down
+        if (snake.direction === "up") return;
+        snake.direction = "down";
+        break;
+
+      case 65:// left
+        if (snake.direction === "right") return;
+        snake.direction = "left";
+        break;
+      case 87:// up
+        if (snake.direction === "down") return;
+        snake.direction = "up";
+        break;
+      case 68:// right
+        if (snake.direction === "left") return;
+        snake.direction = "right";
+        break;
+      case 83:// down
+        if (snake.direction === "up") return;
+        snake.direction = "down";
+        break;
+
+      default:
+        console.log("wrong key");
+    }
+    snake.canTurn = 0;
+  }
+}
+
+// table creation
+function tableCreation() {
+  if (snakeTable.innerHTML === "") {
+    // main table
+    for (var i = 0; i<table.boxes; i++) {
+      var divElt = document.createElement("div");
+      divElt.classList.add("box");
+      snakeTable.appendChild(divElt);
+    }
+    // status bar
+    var statusElt = document.createElement("div");
+    statusElt.classList.add("status");
+    snakeTable.appendChild(statusElt);
+    scoreElt = document.createElement("span");
+    scoreElt.classList.add("score");
+    scoreElt.innerHTML = snake.score + " pts";
+    statusElt.appendChild(scoreElt);
+  }
+}
+
+// swipe Showcase
+$("document").ready(function() {
+  $("body")
+    .swipeDetector()
+    .on("swipeLeft.sd swipeRight.sd swipeUp.sd swipeDown.sd", function(event) {
+      if (event.type === "swipeLeft") {
+        if (snake.direction === "right") return;
+        snake.direction = "left";
+      } else if (event.type === "swipeRight") {
+        if (snake.direction === "left") return;
+        snake.direction = "right";
+      } else if (event.type === "swipeUp") {
+        if (snake.direction === "down") return;
+        snake.direction = "up";
+      } else if (event.type === "swipeDown") {
+        if (snake.direction === "up") return;
+        snake.direction = "down";
+      }
+      snake.canTurn = 0;
+    });
+});
+
